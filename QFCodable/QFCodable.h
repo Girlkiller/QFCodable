51407e3a73150029d66f2b0389a14470cf79017c
qiufeng2 <qiufeng2@yy.com>
2023-11-07 10:26:47 +0800

    Initial Commit

---
+++ QFCodable/QFCodable.h
@@ -0,0 +1,18 @@
+//
+//  QFCodable.h
+//  QFCodable
+//
+//  Created by FengQIU on 2023/11/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QFCodable.
+FOUNDATION_EXPORT double QFCodableVersionNumber;
+
+//! Project version string for QFCodable.
+FOUNDATION_EXPORT const unsigned char QFCodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QFCodable/PublicHeader.h>
+
+
