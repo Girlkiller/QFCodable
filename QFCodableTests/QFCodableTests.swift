b50180838e74bfc21557bd3f2860e1501190e3ee
Girlkiller <137019550@qq.com>
2023-11-17 16:53:09 +0800

    Debug使用系统优化

--- QFCodableTests/QFCodableTests.swift
+++ QFCodableTests/QFCodableTests.swift
@@ -76,6 +76,9 @@ final class QFCodableTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+            for _ in 0..<10000 {
+                _ = QFReusableResponse<[GLLiveListResponse]>.init(from: jsonData)
+            }
         }
     }
 
